705340da6a7840881678848607a1298244df0977
Taguara Digital <youssef.sabbagh@hotmail.com>
2022-03-30 23:02:18 -0400

    change ext names jsx to js

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,7 @@
 // components/layout.js
 
-import Header from "../Header";
-import Footer from "../Footer";
+import Header from "./Header";
+import Footer from "./Footer";
 import Head from "next/head";
 
 export default function Layout({ children }) {
